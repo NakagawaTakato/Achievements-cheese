defad86e99856c0e4dac1b25e0d9af9537d88250
NakagawaTakato <ntakato6231@gmail.com>
2024-04-04 15:11:04 +0900

    3

---
+++ src/app/Http/Controllers/ShopController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Http\Requests\ContactRequest;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function thanks(Request $request)
+    {
+        return view('thanks');
+    }
+
+    public function menu_two(Request $request)
+    {
+        return view('menu_two');
+    }
+
+}
---
+++ src/resources/views/done.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/done.css') }}">
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="container-group">
+        <p>aaa</a>
+    </div>
+</div>
+@endsection
